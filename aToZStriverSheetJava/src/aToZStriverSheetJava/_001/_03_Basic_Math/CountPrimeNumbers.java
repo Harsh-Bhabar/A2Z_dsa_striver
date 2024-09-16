0792f22946ab7b3d73b8a59798077955166316d6
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-09-17 02:40:54 +0530

    count all primes

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_001/_03_Basic_Math/CountPrimeNumbers.java
@@ -0,0 +1,35 @@
+package aToZStriverSheetJava._001._03_Basic_Math;
+
+import java.util.Arrays;
+
+//link - https://leetcode.com/problems/count-primes/
+
+public class CountPrimeNumbers {
+
+    public int countPrimes(int n) {
+        // algo - Sieve of Erathostenes
+        if( n < 2 ){
+            return 0;
+        }
+
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+
+        for(int i=2; i*i < n; i += 1){
+            if(isPrime[i]){
+                for(int j = i*i ; j < n ; j += i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for(int i=2; i<n; i++){
+            if(isPrime[i]){
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+}
