55bbfd9e37a58890a871e8cafdf0dc3ba0166d09
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-09-17 02:06:14 +0530

    perfect num

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_001/_03_Basic_Math/PerfectNumber.java
@@ -0,0 +1,22 @@
+package aToZStriverSheetJava._001._03_Basic_Math;
+
+//link - https://leetcode.com/problems/perfect-number/
+
+public class PerfectNumber {
+
+    public boolean checkPerfectNumber(int num) {
+        int divSum = 0;
+
+        for(int i=1; i<num; i++){
+            if(num % i == 0){
+                divSum += i;
+            }
+            if(divSum > num){
+                return false;
+            }
+        }
+
+        return divSum == num;
+    }
+
+}
