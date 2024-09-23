318ed93a4e2cdf251ffcd219f55da9c331b20e0a
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-09-24 01:10:45 +0530

    recursion basics

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_001/_04_Basic_Recursion/PrintOneToN.java
@@ -0,0 +1,17 @@
+package aToZStriverSheetJava._001._04_Basic_Recursion;
+
+public class PrintOneToN {
+
+    public static void printUptoN(int n){
+        if (n == 0){
+            return;
+        } else{
+          printUptoN(n-1);
+          System.out.println(n);
+        }
+    }
+
+    public static void main(String[] args){
+        printUptoN(6);
+    }
+}
