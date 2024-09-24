90a7cebe86705deb5d2760b78e4c42c9653e172d
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-09-24 22:03:29 +0530

    Fibonacci series

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_001/_04_Basic_Recursion/FibonacciNumber.java
@@ -0,0 +1,26 @@
+package aToZStriverSheetJava._001._04_Basic_Recursion;
+
+import java.util.HashMap;
+
+public class FibonacciNumber {
+
+    private static HashMap<Integer, Integer> memo = new HashMap<>();
+
+    public static int calculateFib(int n){
+        if(n == 0 || n == 1){
+            return n;
+        }
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+
+        int result = calculateFib(n-1) + calculateFib(n-2);
+        memo.put(n, result);
+
+        return result;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Fib - " + calculateFib(25));
+    }
+}
