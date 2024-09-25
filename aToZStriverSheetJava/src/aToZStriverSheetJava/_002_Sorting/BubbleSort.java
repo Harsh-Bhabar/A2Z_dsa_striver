3ad34237880c7ffe5732a342ef6526432e416fad
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-09-26 01:04:49 +0530

    Bubble sort

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_002_Sorting/BubbleSort.java
@@ -0,0 +1,21 @@
+package aToZStriverSheetJava._002_Sorting;
+
+public class BubbleSort {
+    public static void main(String[] args){
+        int[] nums = {1, 5, 6, 6, 2, 0};
+
+        for(int i=1; i<nums.length; i++){
+            for(int j=0; j<nums.length-1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+
+        for(int i: nums){
+            System.out.print(i + " ");
+        }
+    }
+}
