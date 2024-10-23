27885cf8456c44dc0761c73c42a823ff4f62cb11
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-10-23 23:27:04 +0530

    rotate array

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_003_Arrays/Easy/RotateArrayRight.java
@@ -0,0 +1,28 @@
+package aToZStriverSheetJava._003_Arrays.Easy;
+
+//link - https://leetcode.com/problems/rotate-array/description/
+
+public class RotateArrayRight {
+    public void reverse(int[] nums, int start, int end){
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+
+        if(k == 0){
+            return;
+        }
+
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
+    }
+}
