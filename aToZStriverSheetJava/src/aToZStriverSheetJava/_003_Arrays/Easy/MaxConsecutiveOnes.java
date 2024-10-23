42770943b8f4675785f075e29ac1bed86e6f1abf
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-10-24 01:06:07 +0530

    max consec ones

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_003_Arrays/Easy/MaxConsecutiveOnes.java
@@ -0,0 +1,22 @@
+package aToZStriverSheetJava._003_Arrays.Easy;
+
+//link - https://leetcode.com/problems/max-consecutive-ones/description/
+
+public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int ans = 0;
+        int cnt = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 1){
+                cnt++;
+            }else{
+                // last occurance of 1 wont go here
+                if(cnt > ans){
+                    ans = cnt;
+                }
+                cnt = 0;
+            }
+        }
+        return cnt > ans ? cnt : ans;
+    }
+}
