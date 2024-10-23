8d1872e4dedf26248918543bbbbad64f657e8637
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-10-23 18:32:26 +0530

    check for sorted array

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_003_Arrays/Easy/CheckSortedArray.java
@@ -0,0 +1,24 @@
+package aToZStriverSheetJava._003_Arrays.Easy;
+
+//link - https://takeuforward.org/strivers-a2z-dsa-course/strivers-a2z-dsa-course-sheet-2
+
+public class CheckSortedArray {
+    public boolean check(int[] nums) {
+        int n = nums.length;
+
+        if(n == 1){
+            return true;
+        }
+
+        int cntBreak = 0;
+        for(int i=0; i<n; i++){
+            if(nums[(i+1)%n] < nums[i]){
+                cntBreak++;
+            }
+            if(cntBreak > 1){
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- aToZStriverSheetJava/src/aToZStriverSheetJava/_003_Arrays/Easy/SecondLargest.java
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_003_Arrays/Easy/SecondLargest.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+//link - https://www.geeksforgeeks.org/problems/second-largest3735/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+
 public class SecondLargest {
 
 //    method - 1
