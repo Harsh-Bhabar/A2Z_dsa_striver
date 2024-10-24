06ce785ac3cc77c3124dad2a08647ec3dd3e9b5d
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-10-24 23:14:30 +0530

    longest subarray with sum k

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_003_Arrays/Easy/LongestSubarrayWithSumK.java
@@ -0,0 +1,31 @@
+package aToZStriverSheetJava._003_Arrays.Easy;
+
+import java.util.HashMap;
+
+//link - https://www.geeksforgeeks.org/problems/longest-sub-array-with-sum-k0809/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=longest-sub-array-with-sum-k
+
+public class LongestSubarrayWithSumK {
+    public static int lenOfLongSubarr(int nums[], int N, int k) {
+        int pSum = 0;
+        int maxLen = 0;
+        HashMap<Integer, Integer> pMap = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            pSum += nums[i];
+
+            if(pSum == k){
+                maxLen = i+1;
+            }
+
+            if(pMap.containsKey(pSum-k)){
+                maxLen = Math.max(maxLen, i-pMap.get(pSum-k));
+            }
+
+            if(!pMap.containsKey(pSum)){
+                pMap.put(pSum, i);
+            }
+        }
+
+        return maxLen;
+    }
+}
