70a5267d83a81e6351bc6432af5a58faa3ec307d
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-10-30 23:43:30 +0530

    subarray with sum K

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_003_Arrays/Medium/SubarrayWithSumK.java
@@ -0,0 +1,30 @@
+package aToZStriverSheetJava._003_Arrays.Medium;
+
+import java.util.HashMap;
+
+//link - https://leetcode.com/problems/subarray-sum-equals-k/
+
+public class SubarrayWithSumK {
+    public int subarraySum(int[] nums, int k) {
+        int ans = 0;
+        int sum = 0;
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+
+            if(sum == k){
+                ans++;
+            }
+
+            if(mp.containsKey(sum-k)){
+                ans += mp.get(sum-k);
+            }
+
+            mp.put(sum, mp.getOrDefault(sum, 0)+1);
+        }
+
+        return ans;
+    }
+}
