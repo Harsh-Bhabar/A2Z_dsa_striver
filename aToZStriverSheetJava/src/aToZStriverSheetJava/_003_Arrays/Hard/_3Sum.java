f0f436ffb93ea8ac53d7a95c29271e6823357650
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-11-02 18:21:54 +0530

    3 sum

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_003_Arrays/Hard/_3Sum.java
@@ -0,0 +1,43 @@
+package aToZStriverSheetJava._003_Arrays.Hard;
+
+import java.util.*;
+
+public class _3Sum {
+    class Solution {
+        public List<List<Integer>> threeSum(int[] nums) {
+            List<List<Integer>> ans = new ArrayList<>();
+            int n = nums.length;
+
+            Arrays.sort(nums);
+
+            for(int i=0; i<n; i++){
+                if(i != 0 && nums[i] == nums[i-1]){
+                    continue; // skip duplicates
+                }
+
+                int j=i+1;
+                int k=n-1;
+
+                while(j < k){
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if(sum > 0){
+                        k--;
+                    }
+                    else if(sum < 0){
+                        j++;
+                    }
+                    else{
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                        j++;
+                        k--;
+                        // skip duplicates
+                        while(j < k && nums[j] == nums[j-1]) j++;
+                        while(j < k && nums[k] == nums[k+1]) k--;
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
