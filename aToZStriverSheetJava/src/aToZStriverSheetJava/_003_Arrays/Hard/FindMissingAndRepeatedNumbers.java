4fe560922343606f029bedf503d2f900db31a956
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-11-04 23:33:59 +0530

    missing and repeated nums

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_003_Arrays/Hard/FindMissingAndRepeatedNumbers.java
@@ -0,0 +1,61 @@
+package aToZStriverSheetJava._003_Arrays.Hard;
+
+public class FindMissingAndRepeatedNumbers {
+    // hashing
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n = grid[0].length;
+        boolean[] map = new boolean[n*n + 2];
+        int[] ans = new int[2];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                int ele = grid[i][j];
+                if(map[ele] == true){
+                    ans[0] = grid[i][j];
+                }
+                map[ele] = true;
+            }
+        }
+
+        for(int i=1; i<=n*n; i++){
+            if(map[i] == false){
+                ans[1] = i;
+                break;
+            }
+        }
+
+        return ans;
+    }
+    // optimal - Maths
+    public int[] findMissingAndRepeatedValues_optimal(int[][] grid) {
+        int n = grid.length;
+        int N = n * n;
+
+        // Expected sum and sum of squares for numbers 1 to N
+        long expectedSum = (long) N * (N + 1) / 2;
+        long expectedSquareSum = (long) N * (N + 1) * (2 * N + 1) / 6;
+
+        // Actual sum and sum of squares
+        long actualSum = 0;
+        long actualSquareSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int num = grid[i][j];
+                actualSum += num;
+                actualSquareSum += (long) num * num;
+            }
+        }
+
+        // Differences
+        long sumDiff = expectedSum - actualSum;                  // missing - repeated
+        long squareSumDiff = expectedSquareSum - actualSquareSum; // missing^2 - repeated^2
+
+        // Solve equations
+        long sumOfMissingAndRepeated = squareSumDiff / sumDiff;  // missing + repeated
+        int missing = (int) ((sumOfMissingAndRepeated + sumDiff) / 2);
+        int repeated = (int) (sumOfMissingAndRepeated - missing);
+
+        return new int[]{repeated, missing};
+    }
+}
