0927bf87237f6db741c1039363794f45e131d601
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-11-13 19:58:50 +0530

    kth missing number

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_004_BinarySearch/On_Answer/KthMissingNumber.java
@@ -0,0 +1,36 @@
+package aToZStriverSheetJava._004_BinarySearch.On_Answer;
+
+//link - https://leetcode.com/problems/kth-missing-positive-number/
+
+public class KthMissingNumber {
+//    O(n)
+    public int findKthPositive(int[] arr, int k) {
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            if(arr[i] <= k){
+                k++;
+            }else{
+                break;
+            }
+        }
+        return k;
+    }
+//    O(log N)
+    public int findKthPositive_2(int[] nums, int k) {
+        int n = nums.length;
+        int l = 0;
+        int r = n-1;
+
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            int missingAtMid = nums[mid] - (mid+1); // currNum - expectedNum
+            if(missingAtMid < k){
+                l = mid+1;
+            }else{
+                r = mid-1;
+            }
+        }
+        // System.out.println("l - r " + l + " " + r);
+        return l + k;
+    }
+}
