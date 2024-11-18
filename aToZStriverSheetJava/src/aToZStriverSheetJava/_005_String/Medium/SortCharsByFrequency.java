fd7fa1308950cd549b443a7de4954f3237597f94
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-11-18 21:49:41 +0530

    sort chars by freq

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_005_String/Medium/SortCharsByFrequency.java
@@ -0,0 +1,32 @@
+package aToZStriverSheetJava._005_String.Medium;
+
+//link - https://leetcode.com/problems/sort-characters-by-frequency/
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class SortCharsByFrequency {
+    public String frequencySort(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char ch : s.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+        }
+
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>(
+                (a, b) -> b.getValue() - a.getValue()
+        );
+        pq.addAll(map.entrySet());
+
+        StringBuilder ans = new StringBuilder();
+        while(!pq.isEmpty()){
+            Map.Entry<Character, Integer> top = pq.poll();
+            int freq = top.getValue();
+            while(freq-- > 0){
+                ans.append(top.getKey());
+            }
+        }
+
+        return ans.toString();
+    }
+}
