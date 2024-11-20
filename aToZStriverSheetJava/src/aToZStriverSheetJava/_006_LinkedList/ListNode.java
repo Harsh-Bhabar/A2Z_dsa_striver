821cb5546dbadbb2a6fb444ea0362b2d8aa60cad
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-11-20 23:38:39 +0530

    middle of a ll

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_006_LinkedList/ListNode.java
@@ -0,0 +1,10 @@
+package aToZStriverSheetJava._006_LinkedList;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    ListNode(int x){
+        this.val = x;
+        this.next = null;
+    }
+}
