0f3b9b1d49f6d1eac8ba4239871f067f700afc9f
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-11-23 21:50:18 +0530

    intersection of two ll

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_006_LinkedList/Medium/Intersection.java
@@ -0,0 +1,23 @@
+package aToZStriverSheetJava._006_LinkedList.Medium;
+
+import aToZStriverSheetJava._006_LinkedList.ListNode;
+
+//link - https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+public class Intersection {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null){
+            return null;
+        }
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while(a != b){
+            a = (a == null) ? headB : a.next;
+            b = (b == null) ? headA : b.next;
+        }
+
+        return a;
+    }
+}
