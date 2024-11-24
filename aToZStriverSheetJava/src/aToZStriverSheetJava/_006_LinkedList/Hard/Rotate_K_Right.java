78955b34a2d8519c2f6c5d8cd22167fe73911dbe
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-11-24 19:27:39 +0530

    rotate k places right

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_006_LinkedList/Hard/Rotate_K_Right.java
@@ -0,0 +1,39 @@
+package aToZStriverSheetJava._006_LinkedList.Hard;
+
+import aToZStriverSheetJava._006_LinkedList.ListNode;
+
+//link - https://leetcode.com/problems/rotate-list/
+
+public class Rotate_K_Right {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null || k == 0){
+            return head;
+        }
+
+        ListNode curr = head;
+        int len = 1;
+
+        while(curr.next != null){
+            curr = curr.next;
+            len++;
+        }
+
+        if(k % len == 0){
+            return head;
+        }
+
+        int kPlaces = len - (k % len);
+        curr.next = head;
+
+        ListNode temp = head;
+
+        for(int i=1; i<kPlaces; i++){
+            temp = temp.next;
+        }
+
+        head = temp.next;
+        temp.next = null;
+
+        return head;
+    }
+}
