ebcb0da54e10bdb03ae757b66e7f6564d288c650
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-11-25 21:50:49 +0530

    pairs with given sum k

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_006_LinkedList/Medium_DLL/PairsWithGivenSumK.java
@@ -0,0 +1,39 @@
+package aToZStriverSheetJava._006_LinkedList.Medium_DLL;
+
+import aToZStriverSheetJava._006_LinkedList.DLLNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PairsWithGivenSumK {
+    public static ArrayList<ArrayList<Integer>> findPairsWithGivenSum(int target, DLLNode head) {
+        if(head == null || head.next == null){
+            return new ArrayList<>();
+        }
+
+        DLLNode left = head;
+        DLLNode right = head;
+        while(right.next != null){
+            right = right.next;
+        }
+
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+
+        while(left != right && left.prev != right){
+            int sum = left.val + right.val;
+            if(sum == target){
+                ans.add(new ArrayList(Arrays.asList(left.val, right.val)));
+                left = left.next;
+                right = right.prev;
+            }
+            else if(sum < target){
+                left = left.next;
+            }
+            else{
+                right = right.prev;
+            }
+        }
+
+        return ans;
+    }
+}
