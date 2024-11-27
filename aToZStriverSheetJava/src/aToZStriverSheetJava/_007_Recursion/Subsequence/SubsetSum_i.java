6c05311acac15b2b9fdf7132ec4703d289ddef2c
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-11-27 23:25:31 +0530

    subset sum i

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_007_Recursion/Subsequence/SubsetSum_i.java
@@ -0,0 +1,23 @@
+package aToZStriverSheetJava._007_Recursion.Subsequence;
+
+import java.util.ArrayList;
+
+//link - https://www.geeksforgeeks.org/problems/subset-sums2234/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=subset-sums
+
+public class SubsetSum_i {
+    ArrayList<Integer> ans = new ArrayList<>();
+
+    public void helper(int i, int[] nums, int sum){
+        if(i == nums.length){
+            ans.add(sum);
+            return;
+        }
+        helper(i+1, nums, sum + nums[i]);
+        helper(i+1, nums, sum);
+    }
+
+    public ArrayList<Integer> subsetSums(int[] arr) {
+        helper(0, arr, 0);
+        return ans;
+    }
+}
