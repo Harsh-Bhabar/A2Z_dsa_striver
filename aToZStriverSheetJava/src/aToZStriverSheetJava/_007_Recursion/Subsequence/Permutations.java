4ac6d872668007caaff191ebe44fce74e971f959
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-11-29 00:19:55 +0530

    permuation

--- aToZStriverSheetJava/src/aToZStriverSheetJava/_007_Recursion/Subsequence/Permutations.java
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_007_Recursion/Subsequence/Permutations.java
@@ -5,7 +5,7 @@ import java.util.*;
 //link - https://leetcode.com/problems/permutations/
 
 public class Permutations {
-
+ // way - 1
     List<List<Integer>> ans = new ArrayList<>();
 
     public void helper(int[] nums, List<Integer> temp, boolean[] taken){
@@ -31,4 +31,39 @@ public class Permutations {
         helper(nums, temp, taken);
         return ans;
     }
+
+//    way 2
+
+
+//    List<List<Integer>> ans = new ArrayList<>();
+
+    public void helper2(int index, int[] nums){
+        if(index == nums.length){
+            List<Integer> temp = new ArrayList<>();
+            for(int num: nums){
+                temp.add(num);
+            }
+            ans.add(temp);
+            return;
+        }
+
+        for(int i=index; i<nums.length; i++){
+            swap(i, index, nums);
+            helper2(index+1, nums);
+            swap(i, index, nums);
+        }
+    }
+
+    public void swap(int i, int j, int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public List<List<Integer>> permute2(int[] nums) {
+        helper2(0, nums);
+        return ans;
+    }
+
+
 }
