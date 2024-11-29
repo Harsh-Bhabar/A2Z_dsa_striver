ee5d7b81d74a589141a78932c40d3c92db61a4c7
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-11-29 21:03:06 +0530

    valid sudoku

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_007_Recursion/All/validSudoku.java
@@ -0,0 +1,44 @@
+package aToZStriverSheetJava._007_Recursion.All;
+
+public class validSudoku {
+        public boolean solve(char[][] board){
+            for(int i=0; i<9; i++){
+                for(int j=0; j<9; j++){
+                    if(board[i][j] == '.'){
+                        for(char ch = '1'; ch <= '9'; ch++){
+                            if(isValid(board, i, j, ch)){
+                                board[i][j] = ch;
+                                if(solve(board)){
+                                    return true;
+                                }else{
+                                    board[i][j] = '.';
+                                }
+                            }
+                        }
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public boolean isValid(char[][] board, int row, int col, char ch){
+            for(int i=0; i<9; i++){
+                if(board[row][i] == ch){
+                    return false;
+                }
+                if(board[i][col] == ch){
+                    return false;
+                }
+                if(board[3 * (row/3) + i/3][3 * (col/3) + i % 3] == ch){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public boolean isValidSudoku(char[][] board) {
+            return solve(board);
+        }
+
+}
