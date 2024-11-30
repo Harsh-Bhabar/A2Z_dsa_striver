7b197e561b756e6d0c09f5721e191bc9f065d22a
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-11-30 18:54:06 +0530

    kth smallest

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_008_Heap/KthSmallest.java
@@ -0,0 +1,20 @@
+package aToZStriverSheetJava._008_Heap;
+
+import java.util.PriorityQueue;
+
+//link - https://www.geeksforgeeks.org/problems/kth-smallest-element5635/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=kth-smallest-element
+
+public class KthSmallest {
+    public static int kthSmallest(int[] arr, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b-a);
+
+        for(int i=0; i<arr.length; i++){
+            pq.offer(arr[i]);
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+
+        return pq.peek();
+    }
+}
