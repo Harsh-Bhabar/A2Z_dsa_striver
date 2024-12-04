f79fcf5f22fd778ff29cb025e61b73dbd42de8cc
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-12-04 21:47:12 +0530

    max depth of bs

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_010_BinaryTree/Depth_or_Height.java
@@ -0,0 +1,16 @@
+package aToZStriverSheetJava._010_BinaryTree;
+
+//link -https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+public class Depth_or_Height {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+
+        int lh = maxDepth(root.left);
+        int rh = maxDepth(root.right);
+
+        return 1+Math.max(lh, rh);
+    }
+}
