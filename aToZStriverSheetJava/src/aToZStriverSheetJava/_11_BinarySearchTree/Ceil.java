a04e1aa37a2780f66b5bc21d2f8752714093ec36
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-12-06 21:52:51 +0530

    ceil in a bst

--- aToZStriverSheetJava/src/aToZStriverSheetJava/_11_BinarySearchTree/Ceil.java
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_11_BinarySearchTree/Ceil.java
@@ -5,26 +5,22 @@ import aToZStriverSheetJava._010_BinaryTree.TreeNode;
 //link - https://www.geeksforgeeks.org/problems/implementing-ceil-in-bst/1
 
 public class Ceil {
-    int ceil = -1;
-
-    int solve(TreeNode root, int key){
-        if(root == null){
-            return ceil;
-        }
-        if(root.val == key){
-            ceil = key;
-            return ceil;
-        }
-        if(root.val > key){
-            ceil = root.val;
-            return solve(root.left, key);
-        }
-        return solve(root.right, key);
-    }
-
     int findCeil(TreeNode root, int key) {
         if (root == null) return -1;
         // Code here
-        return solve(root, key);
+        int ans = -1;
+        while(root != null){
+            if(root.val == key){
+                return key;
+            }
+            if(root.val > key){
+                ans = root.val;
+                root = root.left;
+            }else{
+                root = root.right;
+            }
+        }
+
+        return ans;
     }
 }
