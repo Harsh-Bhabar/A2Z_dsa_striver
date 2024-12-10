78d6907c7bd0cc149aed150d07200e3cd09b3187
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-12-10 23:00:21 +0530

    fib crazt

--- aToZStriverSheetJava/src/aToZStriverSheetJava/_13_DP/Fibonacci.java
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_13_DP/Fibonacci.java
@@ -42,4 +42,24 @@ public class Fibonacci {
 
         return dp[n];
     }
+
+    // even more optimized
+
+
+    public int fib_3(int n) {
+        if(n <= 1){
+            return n;
+        }
+
+        int prev2 = 0;
+        int prev = 1;
+
+        for(int i=2; i<=n; i++){
+            int curr = prev2 + prev;
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return prev;
+    }
 }
