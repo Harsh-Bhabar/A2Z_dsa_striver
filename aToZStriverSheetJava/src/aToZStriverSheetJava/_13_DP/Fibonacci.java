3f6440293b994a3de29fcc5caaa43e442a13d15e
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-12-10 22:55:18 +0530

    top-down fib

--- aToZStriverSheetJava/src/aToZStriverSheetJava/_13_DP/Fibonacci.java
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_13_DP/Fibonacci.java
@@ -3,6 +3,8 @@ package aToZStriverSheetJava._13_DP;
 import java.util.Arrays;
 
 public class Fibonacci {
+
+    // memorize - bottom-up
     int[] dp;
 
     public int calculate(int n){
@@ -19,5 +21,25 @@ public class Fibonacci {
         dp = new int[n+1];
         Arrays.fill(dp, -1);
         return calculate(n);
+    }
+
+    // top-down
+
+    public int fib_2(int n) {
+        if(n <= 1){
+            return n;
+        }
+
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 }
