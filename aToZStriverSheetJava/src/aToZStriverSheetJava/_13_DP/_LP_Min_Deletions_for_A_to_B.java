c5c89068e4b3697e7fd100088bdb7d938166fd9c
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2024-12-18 01:10:36 +0530

    min deletions for a to b

---
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_13_DP/_LP_Min_Deletions_for_A_to_B.java
@@ -0,0 +1,26 @@
+package aToZStriverSheetJava._13_DP;
+
+//link - https://leetcode.com/problems/delete-operation-for-two-strings/
+
+public class _LP_Min_Deletions_for_A_to_B {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+
+        int[][] dp = new int[m+1][n+1];
+
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        int lcs = dp[m][n];
+
+        return Math.abs(lcs-m) + Math.abs(lcs-n);
+    }
+}
