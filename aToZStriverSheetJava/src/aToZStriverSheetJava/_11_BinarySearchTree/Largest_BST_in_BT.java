78ef508366275cf6c9a1cfe01c0daae134df7d6d
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2025-01-01 00:45:40 +0530

    largest bst sum in bt

--- aToZStriverSheetJava/src/aToZStriverSheetJava/_11_BinarySearchTree/Largest_BST_in_BT.java
+++ aToZStriverSheetJava/src/aToZStriverSheetJava/_11_BinarySearchTree/Largest_BST_in_BT.java
@@ -6,54 +6,57 @@ import aToZStriverSheetJava._010_BinaryTree.TreeNode;
 
 public class Largest_BST_in_BT {
 
-    class Node{
-        int size;
+    class NodeInfo{
         int sum;
-        int max;
+        int size;
         int min;
-        Node(int size, int sum, int max, int min){
-            this.size = size;
+        int max;
+        boolean isBST ;
+        NodeInfo(int sum, int size, int min, int max, boolean isBST){
             this.sum = sum;
-            this.max = max;
+            this.size = size;
             this.min = min;
+            this.max = max;
+            this.isBST = isBST;
         }
     }
 
-    int maxSum = 0;
+    class Solution {
+        int maxSum = 0;
+        int maxSize = 0;
 
-    public Node solve(TreeNode node){
-        if(node == null){
-            return new Node(0, 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
-        }
-        Node left = solve(node.left);
-        Node right = solve(node.right);
-        // for a valid BST -> largest from left < node < smallest from right
-        if(left.max < node.val && node.val < right.min){
-            maxSum = Math.max(maxSum, left.sum + node.val + right.sum);
+        public NodeInfo solve(TreeNode root){
+            if(root == null){
+                return new NodeInfo(0, 0, Integer.MAX_VALUE, Integer.MIN_VALUE, true);
+            }
 
-            return new Node(
-                    1 + left.size + right.size, // size
-                    node.val + left.sum + right.sum, // sum
-                    Math.max(right.max, node.val), // max
-                    Math.min(left.min, node.val) // min
-            );
-        }else{
-            return new Node(
-                    Math.max(left.size, right.size), // size
-                    0, // sum
-                    Integer.MAX_VALUE, // max
-                    Integer.MIN_VALUE // min
-            );
-        }
-    }
+            NodeInfo left = solve(root.left);
+            NodeInfo right = solve(root.right);
 
-    public int maxSumBST(TreeNode root) {
-        // POST ORDER
-        if(root == null){
-            return 0;
+            if(left.isBST && right.isBST && left.max < root.val && root.val < right.min){
+                int currSum = left.sum + root.val + right.sum;
+                int currSize = 1 + left.size + right.size;
+                maxSum = Math.max(maxSum, currSum);
+                return new NodeInfo(
+                        currSum,
+                        currSize,
+                        Math.min(root.val, left.min),
+                        Math.max(root.val, right.max),
+                        true);
+            }else{
+                return new NodeInfo(
+                        0,
+                        Math.max(left.size, right.size),
+                        0,
+                        0,
+                        false
+                );
+            }
         }
-        Node ans = solve(root);
-        // if we need max size of BST we can just do ans.size
-        return maxSum;
+
+        public int maxSumBST(TreeNode root) {
+            solve(root);
+            return maxSum;
+        }
     }
 }
